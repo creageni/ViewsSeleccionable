4cfe00097f5a71a22ba0b6435d56585be78ebd05
Luis Rojas <luisrojasmainmail@gmail.com>
2013-08-02 12:14:55 -0500

    Se selecciona un nombre en la lista y se imprime en un campo de texto, utilizando listView

--- src/com/example/viewsselectionable/MainActivity.java
+++ src/com/example/viewsselectionable/MainActivity.java
@@ -3,20 +3,35 @@ package com.example.viewsselectionable;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  implements OnItemClickListener{
+	TextView txt;
+	ListView list;
+	String datos[];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		txt = (TextView) findViewById(R.id.txt);
+		list=(ListView) findViewById(R.id.list);
+		
+		list.setOnItemClickListener(this);
+		
+		datos=getResources().getStringArray(R.array.nombres);
+		
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		txt.setText(datos[arg2]);
+		
 	}
-
 }
